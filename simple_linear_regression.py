a76d6634b70ba3b38d2d8eb84053f2eb1c84caa1
Abhishek IJ <136907156+Abhishek2003ij@users.noreply.github.com>
2026-04-28 21:37:02 +0530

    Include MSE and R2 score evaluation
    Added model evaluation metrics for predictions.

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import mean_squared_error, r2_score
 
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
@@ -29,6 +30,10 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+# Evaluating the model
+print("MSE:", mean_squared_error(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
 
 # Visualising the Training set results
 plt.scatter(X_train, y_train, color = 'red')
